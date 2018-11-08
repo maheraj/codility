536d5036e06b2d65b627661e97ac278aac6858b8
Maheraj Uddin <maheraj.cse@gmail.com>
2018-11-08 23:04:45 +0600

    Create PermMissingElem.java

---
+++ PermMissingElem.java
@@ -0,0 +1,20 @@
+
+//https://app.codility.com/c/run/training3VNXEB-NFY
+class PermMissingElem {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        int l = A.length;
+        int nl = l + 1;
+        int[] nA = new int[nl];
+        
+        for(int i = 0; i < l; i++) {
+            nA[A[i] -1] ++; 
+        }
+        for(int i = 0; i < nl; i++) {
+            if(nA[i] == 0) {
+                return i+1;
+            }
+        }
+        return 0;
+    }
+}
