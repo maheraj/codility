7c630ef3d861daa3529d08bcb746ed32e5d5eb58
Maheraj Uddin <maheraj.cse@gmail.com>
2018-11-08 22:03:15 +0600

    Create OddOccurrencesInArray.java

---
+++ OddOccurrencesInArray.java
@@ -0,0 +1,22 @@
+//https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
+class OddOccurrencesInArray {
+    public int solution(int[] A) {
+		int result;
+        Arrays.sort(A);
+		int prevNumber = A[0];
+		int count = 1;
+		for(int i = 1; i < A.length; i++) {			
+			if(A[i] == prevNumber) {
+				count++;
+			} else {	
+				if(count % 2 == 1) {
+					break;
+				} else {
+					prevNumber = A[i];
+					count = 1;
+				}
+			}
+		}
+		return prevNumber;
+    }
+}
