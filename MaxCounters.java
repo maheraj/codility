c22e070258daa262e1eaf4ff7b8a3329cc4d2b6c
Maheraj Uddin <maheraj.cse@gmail.com>
2018-11-10 01:29:37 +0600

    Create MaxCounters.java

---
+++ MaxCounters.java
@@ -0,0 +1,31 @@
+// https://app.codility.com/programmers/lessons/4-counting_elements/max_counters/
+
+class MaxCounters {
+    public int[] solution(int N, int[] A) {
+        // write your code in Java SE 8
+        int[] B = new int[N];
+        int max = 0;
+        int base = 0;
+        for(int i = 0; i < A.length; i++) {
+            if(A[i] <= N) {
+                if(B[A[i] -1] <= base) {
+                  B[A[i] -1] = base + 1;
+                } else {
+                  B[A[i] -1]++;
+                }
+                
+                if(max < B[A[i] -1]) {
+                    max = B[A[i] -1];
+                }
+            } else {
+              base = max;
+            }
+        }
+        for(int i = 0; i < B.length; i++) {
+          if(B[i] < base) {
+            B[i] = base;
+          }
+        }
+        return B;
+    }
+}
