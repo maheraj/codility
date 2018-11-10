7599acbba4026135931582ff900b83a8d43f573c
Maheraj Uddin <maheraj.cse@gmail.com>
2018-11-10 21:39:27 +0600

    Update GenomicRangeQuery.java

--- GenomicRangeQuery.java
+++ GenomicRangeQuery.java
@@ -1,8 +1,6 @@
 // https://app.codility.com/programmers/lessons/5-prefix_sums/genomic_range_query/
 // https://app.codility.com/demo/results/trainingZCW8BK-DJR/
 // Correctness 100%, Performance 0%
-
-
 class GenomicRangeQuery {
     public int[] solution(String S, int[] P, int[] Q) {
         // write your code in Java SE 8
@@ -25,6 +23,55 @@ class GenomicRangeQuery {
                 }
             }
             result[i] = min;
+        }
+        return result;
+    }
+}
+
+
+// https://app.codility.com/programmers/lessons/5-prefix_sums/genomic_range_query/
+// https://app.codility.com/demo/results/trainingUH668G-XZ8/
+// 100%
+
+class Solution {
+    public int[] solution(String S, int[] P, int[] Q) {
+        // write your code in Java SE 8
+        int[][] G = new int[4][S.length() + 1];
+        for(int i = 0; i < S.length(); i++) {
+            short a = 0;
+            short c = 0;
+            short g = 0;
+            short t = 0;
+            
+            if(S.charAt(i) == 'A') {
+                a = 1;
+            } else if(S.charAt(i) == 'C') {
+                c = 1;
+            } else if(S.charAt(i) == 'G') {
+                g = 1;
+            } else if(S.charAt(i) == 'T') {
+                t = 1;
+            }
+            G[0][i+1] = G[0][i] + a;
+            G[1][i+1] = G[1][i] + c;
+            G[2][i+1] = G[2][i] + g;
+            G[3][i+1] = G[3][i] + t;
+        }
+        
+        int[] result = new int[P.length];
+        
+        for(int i = 0; i < P.length; i++) {
+            int from = P[i];
+            int to = Q[i] + 1;
+            if(G[0][to] - G[0][from] > 0) {
+                result[i] = 1;
+            } else if(G[1][to] - G[1][from] > 0) {
+                result[i] = 2;
+            } else if(G[2][to] - G[2][from] > 0) {
+                result[i] = 3;
+            } else if(G[3][to] - G[3][from] > 0) {
+                result[i] = 4;
+            }
         }
         return result;
     }
