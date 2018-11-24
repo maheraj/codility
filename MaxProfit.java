da74cfd06999b2620860be6d82e3b33fbc833549
Maheraj Uddin <maheraj.cse@gmail.com>
2018-11-25 00:31:31 +0600

    Create MaxProfit.java

---
+++ MaxProfit.java
@@ -0,0 +1,37 @@
+//https://app.codility.com/programmers/lessons/9-maximum_slice_problem/max_profit/
+//https://app.codility.com/demo/results/training8MS4T4-9SF/
+
+class MaxProfit {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        if(A == null || A.length == 0) {
+            return 0;
+        }
+        
+        int min = A[0];
+        int max = A[0];
+        int profit = 0;
+        
+        int low = A[0];
+        int high = A [0];
+        
+        for(int i = 1; i < A.length; i++) {
+            if(A[i] > high) {
+                high = A[i];
+            } else if(A[i] < low) {
+                if(profit < high - low) {
+                    min = low;
+                    max = high;
+                    profit = max - min;                    
+                }
+                low = high = A[i];
+            }
+        }
+        if(profit < high - low) {
+            min = low;
+            max = high;
+            profit = max - min;
+        }
+        return profit;
+    }
+}
