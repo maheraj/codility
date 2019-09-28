b311acfc8f9e8b6f11085ff6006f5b309e16697a
Maheraj Uddin <maheraj.cse@gmail.com>
2019-09-29 01:36:19 +0600

    Update CyclicRotation.java

--- src/java/main/com/fosociety/codility/CyclicRotation.java
+++ src/java/main/com/fosociety/codility/CyclicRotation.java
@@ -2,6 +2,8 @@ package com.fosociety.codility;
 
 //https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
 class CyclicRotation { 
+	
+	//Solution 1
 	public int[] solution(int[] A, int K) {
 		if(A == null || A.length == 0) {
 			return A;
@@ -25,4 +27,17 @@ class CyclicRotation {
 		}
 		return A;
 	}
+	
+	//Solution 2
+    public int[] solution(int[] A, int K) {
+        // write your code in Java SE 8
+        int[] res = new int[A.length];
+        int k = K % A.length;
+        if (k == 0) return A;
+        for (int i = 0; i < A.length; i++) {
+            res[(i + k) % A.length] = A[i];
+        }
+        return res;
+    }
+	
 }
